7e6531f3ce9329255ff353611e33c7de546bf0c5
kerkkankin-boop <kerkkan.kin@spumail.net>
2026-02-16 18:49:23 +0700

    Add files via upload

---
+++ lab7/projectS/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>ICT12367 SPU<h1>")
+
+def about(request):
+    return HttpResponse("<h1>ICT12367 SPU<h1>")
+
+def form(request):
+    return HttpResponse("<h1>ICT12367 SPU<h1>")
